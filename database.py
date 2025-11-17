4ce2e6b7f49754ec59c1dc04acdff5c12367149f
bandeira13 <malubsouza@yahoo.com.br>
2025-11-17 13:55:03 -0300

    Implementando o banco de dados

---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+def get_connection():
+    conn = sqlite3.connect("tarefas.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+    def init_db():
+        conn = get_connection()
+        conn.execute ("""
+            CREATE TABLE IF NOT EXISTS tarefas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                titulo TEXT NOT NULL,
+                concluida INTEGER DEFAULT 0
+            )
+       """ )    
+        conn.commit()
+        conn.close()
+            
