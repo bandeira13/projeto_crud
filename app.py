724827a066f5c326148e9d8937d29ca236a16eb9
bandeira13 <malubsouza@yahoo.com.br>
2025-11-17 14:32:07 -0300

    Consertando o código

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from database import get_connection, init_db
+
+app = Flask(_name_)
+init_db()
+
+@app.post("/tarefas")
+def criar_tarefa():
+    data = request.json
+    conn = get_connection()
+    conn.execute("INSERT INTO tarefas (titulo) VALUES (?)", (data["titulo"],))
+    conn.commit()
+    return jsonify({"message": "Tarefa criada"}), 201
+
+@app.get("/tarefas")
+def listar_tarefas():
+    conn = get_connection()
+    tarefas = conn.execute("SELECT * FROM tarefas").fetchall()
+    return jsonify([dict(t) for t in tarefas])
+
+@app.put("/tarefas/<int:id>")
+def atualizar_tarefa(id):
+    conn = get_connection()
+    conn.execute("UPDATE tarefas SET concluida = 1 WHERE id = ?", (id,))
+    conn.commit()
+    return jsonify({"message": "Tarefa concluída"})
+
+@app.delete("/tarefas/<int:id>")
+def apagar_tarefa(id):
+    conn = get_connection()
+    conn.execute("DELETE FROM tarefas WHERE id = ?", (id,))
+    conn.commit()
+    return jsonify({"message": "Tarefa removida"})
+
+app.run(debug=True)
--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def get_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-    def init_db():
+def init_db():
         conn = get_connection()
         conn.execute ("""
             CREATE TABLE IF NOT EXISTS tarefas (
